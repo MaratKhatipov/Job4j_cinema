c5605dc33aa10f671df9b48d0c92d1167c7176cc
MaratKhatipov <mmmarat86@gmail.com>
2023-01-19 19:10:15 +0500

    Добавил аннотацию @Service

--- src/main/java/ru/job4j/cinema/service/ipml/TicketServiceImpl.java
+++ src/main/java/ru/job4j/cinema/service/ipml/TicketServiceImpl.java
@@ -1,12 +1,13 @@
 package ru.job4j.cinema.service.ipml;
 
+import org.springframework.stereotype.Service;
 import ru.job4j.cinema.model.Ticket;
 import ru.job4j.cinema.repository.TicketRepository;
 import ru.job4j.cinema.service.TicketService;
 
 import java.util.Collection;
 import java.util.Optional;
-
+@Service
 public class TicketServiceImpl implements TicketService {
 
     private final TicketRepository ticketRepository;
