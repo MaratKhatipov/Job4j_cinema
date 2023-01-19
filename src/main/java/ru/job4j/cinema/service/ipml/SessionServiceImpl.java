f09e444dd2c382c28340cc3d4916591a1a5c9fc5
MaratKhatipov <mmmarat86@gmail.com>
2023-01-19 19:07:29 +0500

    Добавления метода получения свободных мест

--- src/main/java/ru/job4j/cinema/service/ipml/SessionServiceImpl.java
+++ src/main/java/ru/job4j/cinema/service/ipml/SessionServiceImpl.java
@@ -2,13 +2,14 @@ package ru.job4j.cinema.service.ipml;
 
 import org.springframework.stereotype.Service;
 import ru.job4j.cinema.model.Session;
+import ru.job4j.cinema.model.Ticket;
 import ru.job4j.cinema.repository.SessionRepository;
+import ru.job4j.cinema.repository.TicketRepository;
 import ru.job4j.cinema.service.SessionService;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.IntStream;
 
 @Service
@@ -17,9 +18,11 @@ public class SessionServiceImpl implements SessionService {
     private final static List<Integer> ROW_NUMBERS = IntStream.rangeClosed(1, 5).boxed().toList();
     private final static List<Integer> CELL_NUMBERS = IntStream.rangeClosed(1, 10).boxed().toList();
     private final SessionRepository store;
+    private final TicketRepository ticketRepository;
 
-    public SessionServiceImpl(SessionRepository store) {
+    public SessionServiceImpl(SessionRepository store, TicketRepository ticketRepository) {
         this.store = store;
+        this.ticketRepository = ticketRepository;
     }
 
     @Override
@@ -51,5 +54,17 @@ public class SessionServiceImpl implements SessionService {
     public List<Integer> getCellNumbers() {
         return new ArrayList<>(CELL_NUMBERS);
 
+    }
+
+    @Override
+    public List<Integer> getFreeCells(int sessionId, int posRow) {
+        List<Integer> cells = getCellNumbers();
+        TicketServiceImpl ticketService = new TicketServiceImpl(ticketRepository);
+        for (Ticket ticket : ticketService.findSessionAndRow(sessionId, posRow)) {
+            if (cells.contains(ticket.getCell())) {
+                cells.remove(Integer.valueOf(ticket.getCell()));
+            }
+        }
+        return cells;
     }
 }
