01043b8fd4499e6a94b0b401cccb3d14aa491663
MaratKhatipov <mmmarat86@gmail.com>
2023-02-10 17:44:22 +0500

    Доработка и исправление багов в TicketRepository

--- src/main/java/ru/job4j/cinema/repository/TicketRepository.java
+++ src/main/java/ru/job4j/cinema/repository/TicketRepository.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.job4j.cinema.model.Session;
 import ru.job4j.cinema.model.Ticket;
 
 import javax.annotation.concurrent.ThreadSafe;
@@ -21,10 +22,14 @@ public class TicketRepository {
     private static final Logger LOG_TICKET_STORE = LoggerFactory.getLogger(TicketRepository.class.getName());
     private final BasicDataSource pool;
 
-    private static final String INSERT = "INSERT INTO ticket(session_id, pos_row, cell, user_id) VALUES (?, ?, ?, ?)";
+    private static final String INSERT = "INSERT INTO ticket(session_id, user_id, pos_row, cell) VALUES (?, ?, ?, ?)";
 
 
-    private static final String FIND = "SELECT FROM ticket WHERE session_id = ? and pos_row = ?";
+    private static final String FIND = "SELECT * FROM ticket WHERE session_id = ? and pos_row = ?";
+
+    private static final String FIND_BY_ID = "select * from ticket where id = ?";
+
+    private final static String DELETE = "DELETE FROM ticket";
 
     public TicketRepository(BasicDataSource pool) {
         this.pool = pool;
@@ -33,15 +38,16 @@ public class TicketRepository {
     public Optional<Ticket> add(Ticket ticket) {
         Optional<Ticket> result = Optional.empty();
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement(INSERT, PreparedStatement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement ps = cn.prepareStatement(INSERT,
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
             ps.setInt(1, ticket.getSessionId());
-            ps.setInt(2, ticket.getRow());
-            ps.setInt(3, ticket.getCell());
-            ps.setInt(4, ticket.getUserId());
+            ps.setInt(2, ticket.getUserId());
+            ps.setInt(3, ticket.getRow());
+            ps.setInt(4, ticket.getCell());
             ps.execute();
-            try (ResultSet id = ps.getGeneratedKeys()) {
-                if (id.next()) {
-                    ticket.setId(1);
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    ticket.setId(rs.getInt("id"));
                 }
                 result = Optional.of(ticket);
             }
@@ -50,6 +56,14 @@ public class TicketRepository {
         }
         return result;
     }
+
+    /**
+     * метод находит в базе данных все купленные билеты в ряду
+     *
+     * @param sessionId - id сессии
+     * @param posRow       - ряд
+     * @return возвращает коллекцию List с оплаченными билетами в ряду
+     */
     public List<Ticket> findSessionAndRow(int sessionId, int posRow) {
         List<Ticket> result = new ArrayList<>();
         try (Connection cn = pool.getConnection();
@@ -59,12 +73,29 @@ public class TicketRepository {
             try (ResultSet it = ps.executeQuery()) {
                 while (it.next()) {
                     result.add(getTicket(it));
+                    LOG_TICKET_STORE.info("добавил билет с рядом " + posRow);
                 }
             }
         } catch (SQLException e) {
             LOG_TICKET_STORE.error("findSessionAndRow, SQLException", e);
         }
         return result;
+    }
+
+    public Ticket findById(int id) {
+        Ticket ticket = new Ticket();
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement(FIND_BY_ID)) {
+            ps.setInt(1, id);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    ticket = getTicket(it);
+                }
+            }
+        } catch (SQLException e) {
+            LOG_TICKET_STORE.error("find by id, SQLException", e);
+        }
+        return ticket;
     }
 
     private Ticket getTicket(ResultSet it) throws SQLException {
@@ -74,5 +105,15 @@ public class TicketRepository {
                 it.getInt("user_id"),
                 it.getInt("pos_row"),
                 it.getInt("cell"));
+    }
+
+    public void reset() {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement(DELETE)
+        ) {
+            ps.execute();
+        } catch (Exception e) {
+            LOG_TICKET_STORE.error("Error:", e);
+        }
     }
 }
